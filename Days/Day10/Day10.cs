09bec68f8941fb35845c06202435e16105c1546c
Jake Baker <SEP.Baker@fronius.com>
2023-05-19 14:23:21 -0400

    Day10, Day11

---
+++ Days/Day10/Day10.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode2018.Utils;
+using JetBrains.Annotations;
+
+namespace AdventOfCode2018.Days.Day10;
+
+[UsedImplicitly]
+public class Day10 : AdventOfCode<long, IReadOnlyList<PV>>
+{
+    public override IReadOnlyList<PV> Parse(string input) => 
+        input.Lines().Select(it => {
+            var m = Regex.Match(it, @"(?<x>-?\d+)[^0-9-]+(?<y>-?\d+)[^0-9-]+(?<dx>-?\d+)[^0-9-]+(?<dy>-?\d+)");
+            return new PV(
+                new Position(m.LongGroup("y"), m.LongGroup("x")),
+                new Vector(m.LongGroup("dy"), m.LongGroup("dx"))
+            );
+        }).ToList();
+
+    [TestCase(Input.File, 0)] 
+    public override long Part1(IReadOnlyList<PV> world)
+    {
+        var lastDy = long.MaxValue;
+        var seconds = 0;
+        while (true)
+        {
+            seconds += 1;
+            var lastWorld = world;
+            world = world.Select(pv => new PV(pv.Position + pv.Vector, pv.Vector)).ToList();
+            var grid = world.Select(pv => pv.Position).ToHashSet();
+            var minX = grid.Select(p => p.X).Min();
+            var maxX = grid.Select(p => p.X).Max();
+            var minY = grid.Select(p => p.Y).Min();
+            var maxY = grid.Select(p => p.Y).Max();
+            var dy = maxY - minY + 1;
+            if (dy > lastDy)
+            {
+                world = lastWorld;
+                grid = world.Select(pv => pv.Position).ToHashSet();
+                Console.WriteLine();
+                for (var y = minY; y <= maxY; y++)
+                {
+                    for (var x = minX; x <= maxX; x++)
+                    {
+                        Console.Write(grid.Contains(new Position(y, x)) ? "." : "#");
+                    }
+                    Console.WriteLine(seconds);
+                }
+                break;
+            }
+            lastDy = dy;
+        }
+        return 0;
+    }
+
+    // [TestCase(Input.Example, 0)]
+    [TestCase(Input.File, 0)]
+    public override long Part2(IReadOnlyList<PV> settings)
+    {
+        return 0;
+    }
+}
+
+public record PV(Position Position, Vector Vector);
---
+++ Days/Day11/Day11.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode2018.Utils;
+using JetBrains.Annotations;
+
+namespace AdventOfCode2018.Days.Day11;
+
+[UsedImplicitly]
+public class Day11 : AdventOfCode<string, long>
+{
+    public override long Parse(string input) => 
+        input.Lines().Select(it => Convert.ToInt64(it)).Single();
+
+    [TestCase(Input.Raw, "33,45", Raw = "18")] 
+    [TestCase(Input.Raw, "21,61", Raw = "42")] 
+    [TestCase(Input.Raw, "243,17", Raw = "7347")] 
+    public override string Part1(long gridSerialNumber)
+    {
+        var grid = new Dictionary<Position, long>();
+        for(var y = 1; y <= 300; y++)
+        {
+            for (var x = 1; x <= 300; x++)
+            {                
+                var pl = PowerLevel(x, y, gridSerialNumber);
+                for (var dx = 0; dx >= -2; dx--)
+                {
+                    for (var dy = 0; dy >= -2; dy--)
+                    {
+                        var p = new Position(y + dy, x + dx);
+                        grid[p] = grid.GetValueOrDefault(p) + pl;
+                    }
+                }
+            }
+        }
+        var item = grid.Where(p => p.Key.X is >= 1 and <= 300 && p.Key.Y is >= 1 and <= 300).MaxBy(kv => kv.Value);
+        return $"{item.Key.X},{item.Key.Y}";
+    }
+
+    [TestCase(Input.Raw, "90,269,16", Raw = "18")] 
+    [TestCase(Input.Raw, "232,251,12", Raw = "42")] 
+    [TestCase(Input.Raw, "233,228,12", Raw = "7347")]
+    public override string Part2(long gridSerialNumber)
+    {
+        var grid = new Dictionary<PositionAndGridSize, long>();
+        var foundPl = long.MinValue;
+        var foundPosition = new PositionAndGridSize(new Position(-1, -1), 0);
+
+        for(var y = 1; y <= 300; y++)
+        {
+            for (var x = 1; x <= 300; x++)
+            {
+                var pl = 0L;
+                for (var gridSize = 1; gridSize <= Math.Min(300 -y + 1, 300 -x + 1); gridSize++)
+                {
+                    // Add right column
+                    for (var dy = 0; dy < gridSize-1; dy++) pl += PowerLevel(x + gridSize - 1, y + dy, gridSerialNumber);
+                    // Add bottom row
+                    for (var dx = 0; dx < gridSize-1; dx++) pl += PowerLevel(x + dx, y + gridSize - 1, gridSerialNumber);
+                    // Add corner
+                    pl += PowerLevel(x + gridSize -1, y + gridSize -1, gridSerialNumber);
+                    if (pl > foundPl)
+                    {
+                        foundPl = pl;
+                        foundPosition = new PositionAndGridSize(new Position(y, x), gridSize);
+                    }
+                }
+            }
+        }
+
+        return $"{foundPosition.Position.X},{foundPosition.Position.Y},{foundPosition.GridSize}";
+    }
+
+    private long PowerLevel(long x, long y, long gridSerialNumber)
+    {
+        var rackId = x + 10;
+        var pl = rackId * y;
+        pl += gridSerialNumber;
+        pl *= rackId;
+        pl = (pl % 1000) / 100;
+        pl -= 5;
+        return pl;
+    }
+}
+
+public record PositionAndGridSize(Position Position, int GridSize);
